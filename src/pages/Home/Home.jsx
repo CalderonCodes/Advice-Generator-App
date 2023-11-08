c941e5909f16d4ae4cc68ddd0b44eaaeac12c35d
CalderonCodes <cgcalderondev@gmail.com>
2023-11-08 01:43:25 -0600

    fix: fix mobile footer :bug:

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='bg-DarkBlue flex justify-center items-center h-screen'>
         <Container/>
-        <div className="flex flex-col montserrat-500 text-center text-sm text-neutral-50 pb-5 lg:pl-5 lg:absolute lg:left-0 lg:bottom-0">
+        <div className="flex flex-col montserrat-500 text-center text-xs lg:text-sm text-neutral-50 pb-5 lg:pl-5 absolute lg:left-0 center-0 bottom-0">
         <div>
         Challenge by <a className="underline text-super-dark-cyan" href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
         </div>
